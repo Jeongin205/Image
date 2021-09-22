2663cfa038bcdf5a89545eb73c7880ed7d5cc35f
Jeongin205 <81507170+Jeongin205@users.noreply.github.com>
2021-09-22 21:48:11 +0900

    주석처리

--- app/src/main/java/com/example/image/MainActivity.java
+++ app/src/main/java/com/example/image/MainActivity.java
@@ -35,7 +35,6 @@ public class MainActivity extends AppCompatActivity implements BottomSheetWriteF
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         sheetWriteFragment = new BottomSheetWriteFragment();
-
         binding.image.setOnClickListener(view -> {
             sheetWriteFragment.show(getSupportFragmentManager(), "bottomSheet");
         });
@@ -43,21 +42,21 @@ public class MainActivity extends AppCompatActivity implements BottomSheetWriteF
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    public void onClickGallery() {
+    public void onClickGallery() {//저장소 읽어오는 퍼미션
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED){
             requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
-        }
+        }//퍼미션이 허용되지 않아 있다면 퍼미션 허용메세지 생성 & 코드0보내기
         else{
-            setGallery();
+            setGallery();//퍼미션이 허용되어 있다면 갤러리불러오기
         }
         sheetWriteFragment.dismiss();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    public void onClickCamera() {
+    public void onClickCamera() {//카메라 퍼미션
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==PackageManager.PERMISSION_DENIED){
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 1);//코드 1보내기
         }
         else{
             setCamera();
@@ -67,25 +66,25 @@ public class MainActivity extends AppCompatActivity implements BottomSheetWriteF
     }
 
     @Override
-    public void onClickBasics() {
+    public void onClickBasics() {//기본이미지 선택했을때 drawable에 있는거 불러오기
         Glide.with(getApplicationContext()).load(R.drawable.ic_profile).into(binding.image);
-        Log.d("asdf", String.valueOf(Uri.parse("android.resource://com.example.jeongstagram/drawable/ic_profile")));
+        Log.d("asdf", String.valueOf(Uri.parse("android.resource://com.example.image/drawable/ic_profile")));
         sheetWriteFragment.dismiss();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
+        //퍼미션허용 메세지를 보냈을떄
         if (Build.VERSION.SDK_INT >= 23) {
 
             // requestPermission의 배열의 index가 아래 grantResults index와 매칭
             // 퍼미션이 승인되면
             if(grantResults.length > 0  && grantResults[0]== PackageManager.PERMISSION_GRANTED){
-                if(requestCode==0){
+                if(requestCode==0){//위에서 코드 0을 보냄
                     setGallery();
                 }
-                else if(requestCode==1){
+                else if(requestCode==1){//코드 1
                     setCamera();
                 }
 
@@ -94,13 +93,13 @@ public class MainActivity extends AppCompatActivity implements BottomSheetWriteF
             else {
                 if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                         || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-
+                    //퍼미션 2번까진 아래코드
                     Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 다시 눌러 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
 
 
                 }else {
                     Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
-
+                    //2번이후론 알아서
                 }
             }
             }
@@ -108,11 +107,10 @@ public class MainActivity extends AppCompatActivity implements BottomSheetWriteF
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 0) {
+        if (requestCode == 0) {//코드 0번 갤러리
             if (resultCode == RESULT_OK) {
                 try {
                     InputStream in = getContentResolver().openInputStream(data.getData());
-
                     Bitmap img = BitmapFactory.decodeStream(in);
                     in.close();
                     Log.d("asdf", getImageUri(this, img).toString());
@@ -124,14 +122,12 @@ public class MainActivity extends AppCompatActivity implements BottomSheetWriteF
                 Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
             }
         }
-        if(requestCode == 1){
+        if(requestCode == 1){//코드 1번 카메라
             if(resultCode == RESULT_OK){
                 try{
                     Bundle extras = data.getExtras();
                     Bitmap imageBitmap = (Bitmap) extras.get("data");
                     Log.d("asdf", getImageUri(this, imageBitmap).toString());
-
-                    // 이미지뷰에 Bitmap으로 이미지를 입력
                     Glide.with(getApplicationContext()).load(imageBitmap).into(binding.image);
                 }catch (Exception e){
 
@@ -139,17 +135,17 @@ public class MainActivity extends AppCompatActivity implements BottomSheetWriteF
             }
         }
     }
+    private void setGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 0);
+    }
      private void setCamera(){
          Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
          startActivityForResult(intent, 1);
      }
-     private void setGallery(){
-         Intent intent = new Intent();
-         intent.setType("image/*");
-         intent.setAction(Intent.ACTION_GET_CONTENT);
-         startActivityForResult(intent, 0);
-     }
-    public Uri getImageUri(Context ctx, Bitmap bitmap) {
+    public Uri getImageUri(Context ctx, Bitmap bitmap) {//비트맵 Uri로 변환
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
         String path = MediaStore.Images.Media.insertImage
